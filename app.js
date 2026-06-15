c15f1e8266db4ffea29160adc086184971e83adb
Atchaya <atchayasp1711gmail.com>
2026-06-15 10:24:33 +0530

    mongodb atlas

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 /* MongoDB Connection */
 
 mongoose.connect(
-"mongodb://127.0.0.1:27017/studentdb"
+"mongodb+srv://atchaya:<db_@atchaya001>@cluster0.ipnkjaq.mongodb.net/?appName=Cluster0"
 )
 
 .then(()=>{
