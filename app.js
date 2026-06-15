b3b9cd3fde69140ea44aef2d082ebca07a9b2777
Atchaya <atchayasp1711gmail.com>
2026-06-15 14:39:22 +0530

    fixed deploy

--- app.js
+++ app.js
@@ -103,7 +103,9 @@ app.delete("/delete/:id", async(req,res)=>{
 
 /* Server */
 
-app.listen(5000,()=>{
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
 
     console.log("Server Running");
 
